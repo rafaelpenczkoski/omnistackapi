302791a09d784dab9d5ec4c329cabb1fbf485689
Rafael Penczkoski <rafaelpenczkoski@gmail.com>
2019-04-10 00:24:58 -0300

    Estrutura backend

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const socket = require('socket.io');
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = socket(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+});
+
+mongoose.connect(
+    'mongodb+srv://omnistack:omnistack@cluster0-pk3tb.mongodb.net/omnistack?retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded( {extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+server.listen(3333);
